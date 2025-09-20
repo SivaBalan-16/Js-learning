126e7453daa2bcb58f4859d201dc0415caac74a4
SivaBalan-16 <sivabalan16102003@gmail.com>
2025-09-20 15:21:21 +0530

    learning crud

---
+++ CRUD in JS/Arr.js
@@ -0,0 +1,5 @@
+let num = []
+let update = [1,2,3,4,5,6]
+
+const newarr = new Array(update)
+console.log(update.length)
---
+++ CRUD in JS/num.js
@@ -0,0 +1,5 @@
+let firstName = "Siva";
+let lastName = "Balan";
+let wel = `Welcome to my site ${firstName} ${lastName}`;
+
+console.log(wel);
