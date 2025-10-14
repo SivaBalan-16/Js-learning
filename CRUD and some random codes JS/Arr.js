f2bb87fef14ecd7afad62e7d4286aefb97d1760c
SivaBalan-16 <sivabalan16102003@gmail.com>
2025-10-14 22:54:55 +0530

    arr

--- CRUD and some random codes JS/Arr.js
+++ CRUD and some random codes JS/Arr.js
@@ -1,8 +1,47 @@
-let fruit =["Apple","Mango","Banana","Orange"]
+// let fruit =["Apple","Mango","Banana","Orange"]
 
-let basket=[]
+// let basket=[]
 
-for(count=0; count<=3;count=count+1){
-        basket.push(fruit[count])
-}
-console.log(basket)
+// for(count=0; count<=3;count=count+1){
+//         basket.push(fruit[count])
+// }
+// console.log(basket)
+
+
+//2D array
+// let martix = [[3,4,5],[4,5,7],[6,7,8]]
+
+//push in array
+// let arr = ["a","b","c","d"]
+// arr.push("e")
+// console.log(arr)
+
+//pop in array & it return the last element
+// let arr = ["a","b","c","d"]
+// let lastElement = arr.pop()
+// console.log(lastElement)
+
+// shift in array it's made the shift the first element 
+// let arr = ["a","b","c","d"]
+// console.log(arr.shift())
+
+//unshift in array it's will add an element in starting of the element
+// let arr = ["a","b","c","d"]
+// arr.unshift(0)
+// console.log(arr)
+
+//splice in array we can use replace,delete & insert with the help of splice methode
+let arr = ["a","b","c","d"]
+
+// arr.splice(0,1)
+// console.log(arr) //delete
+
+// arr.splice(0,1,"A")  //replace
+// console.log(arr)
+
+// arr.splice(0,0,"AA") //add element
+// console.log(arr.length)
+
+
+
+
